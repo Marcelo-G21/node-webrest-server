9322552b0f6c48f62b1d8f2be9b298dc889e63f7
Marcelo-G21 <marcelo.garcia.arenas@gmail.com>
2025-12-10 15:35:25 -0300

    added CRUD DTO and database connection

---
+++ dist/data/postgres/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.prisma = void 0;
+const envs_1 = require("../../config/envs");
+const adapter_pg_1 = require("@prisma/adapter-pg");
+const client_1 = require("../../generated/prisma/client");
+const connectionString = `${envs_1.envs.POSTGRES_URL}`;
+const adapter = new adapter_pg_1.PrismaPg({ connectionString });
+const prisma = new client_1.PrismaClient({ adapter });
+exports.prisma = prisma;
